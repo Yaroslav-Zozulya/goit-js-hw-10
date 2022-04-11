cf81238e956ed24e5da194baa5b4eaa1603c1284
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-04-11 16:25:16 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -6,7 +6,7 @@ export function fetchCountries(name) {
   return fetch(`${URL}${name}?fields=name,capital,population,flags,languages`).then(response => {
     if (!response.ok) {
       Notify.failure('Oops, there is no country with that name');
-      return;
+      return response;
     }
     return response.json();
   });
